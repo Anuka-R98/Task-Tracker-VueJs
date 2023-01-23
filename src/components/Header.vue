28afb532b4c41cb222d98d4af7833be085431706
Anuka <arrinsamurai7@gmail.com>
2023-01-23 16:24:44 +0530

    Components -header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+//scope = only for this specifi component
+<style>
+    header {
+         display: flex;
+         justify-content: space-between;
+         align-items: center;
+         margin-bottom: 20px;
+    }
+</style>
