319f846635534c8c4bd0a153f038ab82108ac59b
Anuka <arrinsamurai7@gmail.com>
2023-01-24 00:10:20 +0530

    finalize

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task '" 
+        <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' : 'Add Task '" 
         :color="showAddTask ? 'red' : 'green'"/>
     </header>
 </template>
@@ -17,6 +17,16 @@ export default {
     },
     components: {
         Button,
+    },
+    // computed property is used for reactivity ( to combine things )
+    computed: {
+        homePage() {
+            if(this.$route.path === '/') {
+                return true
+            } else {
+                return false
+            }
+        }
     }
 }
 </script>
